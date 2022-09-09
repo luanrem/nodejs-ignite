b2cd2e953d106965337f26c603eceab6cc25a7ca
luanrem <luanrem@gmail.com>
2022-09-08 21:03:12 -0300

    Utilizando os metodos HTTP

--- Chapter1/fundamentos-nodejs/src/index.js
+++ Chapter1/fundamentos-nodejs/src/index.js
@@ -2,9 +2,32 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  //   return response.send("Hello World");
-  return response.json({ message: "Hello World Ignite - Fundamentos NodeJS" });
+/**
+ * GET - Buscar uma informação dentro do servidor
+ * POST - Inserir uma informação no servidor
+ * PUT - Alterar uma informação no servidor
+ * PATCH - Alterar uma informação específica
+ * DELETE - Deletar uma informação no servidor
+ */
+
+app.get("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 4"]);
 });
 
 //localhost:3333
