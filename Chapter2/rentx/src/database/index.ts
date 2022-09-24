1456e72a0f9f671a631ed92d9018e045a86e1012
luanrem <luanrem@gmail.com>
2022-09-23 21:10:56 -0300

    recreate the api with previous typeorm version

--- Chapter2/rentx/src/database/index.ts
+++ Chapter2/rentx/src/database/index.ts
@@ -1,12 +1,13 @@
-import { DataSource } from "typeorm";
+import { createConnection, getConnectionOptions } from "typeorm";
 
-const dataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "docker",
-  password: "ignite",
-  database: "rentx",
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions().then((options) => {
+  const newOptions = options as IOptions;
+  newOptions.host = "database_ignite"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({
+    ...options,
+  });
 });
-
-dataSource.initialize();
