1a7e6eafd9d1c2c103612415052a2308fdf2d843
luanrem <luanrem@gmail.com>
2022-09-28 21:53:30 -0300

    Refatorando o caso de uso de categoria

--- Chapter2/rentx/src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ Chapter2/rentx/src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -11,15 +11,8 @@ class CategoriesRepository implements ICategoriesRepository {
 
   private static INSTANCE: CategoriesRepository;
 
-  private constructor() {
+  constructor() {
     this.repository = getRepository(Category);
-  }
-
-  public static getInstance(): CategoriesRepository {
-    if (!CategoriesRepository.INSTANCE) {
-      CategoriesRepository.INSTANCE = new CategoriesRepository();
-    }
-    return CategoriesRepository.INSTANCE;
   }
 
   async create({ description, name }: ICreateCategoryDTO): Promise<void> {
