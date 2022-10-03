ef4ff43754e8a4cc82f9e098770834f4b063960b
luanrem <luanrem@gmail.com>
2022-10-03 12:22:29 -0300

    Adicionando coluna de avatar

--- Chapter2/rentx/src/routes/users.routes.ts
+++ Chapter2/rentx/src/routes/users.routes.ts
@@ -1,11 +1,24 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoute = Router();
 
+const upload = multer({
+  dest: "avatar",
+});
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoute.post("/", createUserController.handle);
+
+usersRoute.patch(
+  "/avatar",
+  upload.single("file"),
+  updateUserAvatarController.handle
+);
 
 export { usersRoute };
