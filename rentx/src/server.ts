5ee8373fc9400ca181b500a1b93b5266aa42d320
luanrem <luanrem@gmail.com>
2022-10-09 12:59:39 -0300

    Imports da aplicacao

--- rentx/src/server.ts
+++ rentx/src/server.ts
@@ -2,9 +2,10 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUI from "swagger-ui-express";
 
-import { AppError } from "./errors/AppError";
+import { AppError } from "@errors/AppError";
+
 import "./database";
-import "./shared/container";
+import "@shared/container";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
