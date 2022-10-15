a61d77822fc560854fae4104dd76f1dc6e7d781b
luanrem <luanrem@gmail.com>
2022-10-14 21:13:44 -0300

    Criando seed de usuario

--- rentx/src/shared/infra/http/server.ts
+++ rentx/src/shared/infra/http/server.ts
@@ -3,12 +3,13 @@ import "express-async-errors";
 import swaggerUI from "swagger-ui-express";
 
 import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm";
 
-import "../typeorm";
 import "@shared/container";
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
+createConnection();
 const app = express();
 
 app.use(express.json());
