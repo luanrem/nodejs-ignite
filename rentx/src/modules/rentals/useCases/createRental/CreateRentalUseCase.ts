8512230e85eab50e16812301bba372b12c6c8663
luanrem <luanrem@gmail.com>
2022-11-06 20:27:45 -0300

    Criando os testes do aluguel

---
+++ rentx/src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,35 @@
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { AppError } from "@shared/errors/AppError";
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+class CreateRentalUseCase {
+  constructor(private rentalsRepository: IRentalsRepository) {}
+
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: IRequest): Promise<void> {
+    // Nao deve ser possivel cadastrar um novo aluguel caso ja exista um aberto para o mesmo carro
+    const carUnavailable = await this.rentalsRepository.findByCar(car_id);
+
+    if (carUnavailable) {
+      throw new AppError("Car is unavailable");
+
+      // Nao deve ser possivel cadastrar um novo aluguel caso ja exista um aberto para o mesmo usuario
+      const rentalOpenToUser =
+        await this.rentalsRepository.findOpenRentalByUser(user_id);
+
+      if (rentalOpenToUser) {
+        throw new AppError("There is a rental in progress for user!");
+      }
+    }
+  }
+}
+
+export { CreateRentalUseCase };
