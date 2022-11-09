7b0d116c3effa55fe9764807585731ad323a6826
luanrem <luanrem@gmail.com>
2022-11-09 19:38:48 -0300

    Configurando supertest

--- rentx/src/shared/infra/http/server.ts
+++ rentx/src/shared/infra/http/server.ts
@@ -1,36 +1,3 @@
-import express, { NextFunction, Request, Response } from "express";
-import "express-async-errors";
-import swaggerUI from "swagger-ui-express";
-
-import { AppError } from "@shared/errors/AppError";
-import createConnection from "@shared/infra/typeorm";
-
-import "@shared/container";
-import swaggerFile from "../../../swagger.json";
-import { router } from "./routes";
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
-
-app.use(router);
-
-app.use(
-  (err: Error, requests: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message,
-      });
-    }
-
-    return response.status(500).json({
-      status: "error",
-      message: `Internal server error - ${err.message}`,
-    });
-  }
-);
+import { app } from "./app";
 
 app.listen(3000, () => console.log("Server is running!"));
