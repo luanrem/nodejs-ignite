16481ba03caa8ca826f5ae9ffd9eece1647e53c7
luanrem <luanrem@gmail.com>
2022-11-16 21:02:34 -0300

    Refatorando Autenticacao do usuario

---
+++ rentx/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "238b781fcd136d42a20e8208943f9072",
+  expires_in_token: "15m",
+  secret_refresh_token: "125c41faf66e2c351bff02def3b22cab",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_day: 30,
+};
--- rentx/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ rentx/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,7 +2,10 @@ import { compare } from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import auth from "@config/auth";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
@@ -16,34 +19,58 @@ interface IResponse {
     email: string;
   };
   token: string;
+  refresh_token: string;
 }
 
 @injectable()
 class AuthenticateUserUseCase {
   constructor(
     @inject("UsersRepository")
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject("DayjsDateProvider")
+    private dateProvider: DayjsDateProvider
   ) {}
 
   async execute({ email, password }: IRequest): Promise<IResponse> {
-    // Usuario existe
     const user = await this.usersRepository.findByEmail(email);
+    const {
+      expires_in_token,
+      secret_refresh_token,
+      secret_token,
+      expires_in_refresh_token,
+      expires_refresh_token_day,
+    } = auth;
 
     if (!user) {
       throw new AppError("Email or password incorrect!");
     }
 
-    // Senha esta correta
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
       throw new AppError("Email or password incorrect!");
     }
 
-    // Gerar jsonwebtoken
-    const token = sign({}, "238b781fcd136d42a20e8208943f9072", {
+    const token = sign({}, secret_token, {
       subject: user.id,
-      expiresIn: "1d",
+      expiresIn: expires_in_token,
+    });
+
+    const refresh_token = sign({ email }, secret_refresh_token, {
+      subject: user.id,
+      expiresIn: expires_in_refresh_token,
+    });
+
+    const refresh_token_expires_date = this.dateProvider.addDays(
+      expires_refresh_token_day
+    );
+
+    await this.usersTokensRepository.create({
+      user_id: user.id,
+      refresh_token,
+      expires_date: refresh_token_expires_date,
     });
 
     const tokenReturn: IResponse = {
@@ -52,6 +79,7 @@ class AuthenticateUserUseCase {
         name: user.name,
         email: user.email,
       },
+      refresh_token,
     };
 
     return tokenReturn;
